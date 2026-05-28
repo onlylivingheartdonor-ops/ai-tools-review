afd9aeb6d40b8fc8feba0c5519981028c860b69c
David M. Graham <onlylivingheartdonor.com>
2026-05-28 10:17:19 -0400

    Auto-fix: regenerate lock file, add www redirect, add ads.txt, fix sitemap

---
+++ .next/server/middleware-build-manifest.js
@@ -0,0 +1,21 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/03~yq9q893hmn.js"
+  ],
+  "lowPriorityFiles": [
+    "static/HA5grbb1CpgnCZZJIPj_9/_buildManifest.js",
+    "static/HA5grbb1CpgnCZZJIPj_9/_ssgManifest.js",
+    "static/HA5grbb1CpgnCZZJIPj_9/_clientMiddlewareManifest.js"
+  ],
+  "rootMainFiles": [
+    "static/chunks/15xrurgzs99gv.js",
+    "static/chunks/0dgq26a5_oy.a.js",
+    "static/chunks/07lhk_q6pmm3r.js",
+    "static/chunks/02g3221oh~3le.js",
+    "static/chunks/turbopack-05poq_ye6dti6.js"
+  ]
+};
